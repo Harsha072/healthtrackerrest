4a156f9fd0168c2c5ecaa879de023872396f09d4
Harsha072 <20100308@mail.wit.ie>
2022-11-04 08:14:15 +0000

    fixed workoutDAOTest

--- src/test/kotlin/ie/setu/repository/WorkoutDAOTest.kt
+++ src/test/kotlin/ie/setu/repository/WorkoutDAOTest.kt
@@ -1,9 +1,10 @@
 package ie.setu.repository
 
-import ie.setu.domain.Activity
-import ie.setu.domain.db.Activities
+
+import ie.setu.domain.Workout
+
 import ie.setu.domain.db.Workouts
-import ie.setu.domain.repository.ActivityDAO
+
 import ie.setu.domain.repository.WorkoutDAO
 import ie.setu.helpers.*
 import junit.framework.TestCase
@@ -35,7 +36,7 @@ class WorkoutDAOTest {
         fun `multiple sessions added to table can be retrieved successfully`() {
             transaction {
 
-                //Arrange - create and populate tables with three users and three activities
+
                 val userDAO = populateUserTable()
                 val workoutDAO = populateWorkoutTable()
                 val workoutSessionDAO = populateWorkoutSessionTable()
@@ -45,170 +46,167 @@ class WorkoutDAOTest {
             }
         }
     }
-//    @Nested
-//    inner class ReadWorkouts {
-//
-//        @Test
-//        fun `getting all workouts stored in table successfully`(){
-//            transaction {
-//                val usersDAO = populateUserTable()
-//                val activityDAO = populateActivityTable()
-//                TestCase.assertEquals(3, activityDAO.getAll().size)
-//            }
-//
-//
-//
-//        }
-//        @Test
-//        fun `get workouts by user id that has no related workouts, results in no record found`(){
-//            transaction {
-//                val usersDAO = populateUserTable()
-//                val activityDAO = populateActivityTable()
-//                TestCase.assertEquals(0, activityDAO.findByUserId(4).size)
-//            }
-//
-//
-//
-//        }
-//
-//        @Test
-//        fun `get workout by user id that has related workout, results in correct workout returned`() {
-//            transaction {
-//                val usersDAO = populateUserTable()
-//               val workoutDAO = populateWorkoutTable()
-//
-//               TestCase.assertEquals(workout1, workoutDAO.findWorkoutByUserId(1).get(0))
-//               TestCase.assertEquals(workout3, workoutDAO.findWorkoutByUserId(1).get(1))
-//
-//
-//
-//            }
-//
-//        }
-//        @Test
-//        fun `get all workout over empty table returns none`() {
-//            transaction {
-//
-//                //Arrange - create and setup activityDAO object
-//                SchemaUtils.create(Workouts)
-//                val workoutDAO = WorkoutDAO()
-//
-//                //Act & Assert
-//                TestCase.assertEquals(0, workoutDAO.getAllWorkouts().size)
-//            }
-//        }
-//        @Test
-//        fun `get workout by workout id that has no records, results in no record returned`() {
-//            transaction {
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val workoutDAO = populateWorkoutTable()
-//                //Act & Assert
-//                TestCase.assertEquals(null, workoutDAO.findByWorkoutId(4))
-//            }
-//        }
-//
-//    }
-//
-//    @Nested
-//    inner class UpdateActivities {
-//
-//        @Test
-//        fun `updating existing activity in table results in successful update`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val activityDAO = populateActivityTable()
-//
-//                //Act & Assert
-//                val activity3updated = Activity(id = 3, description = "Cardio", duration = 42.0,
-//                    calories = 220, started = DateTime.now(), userId = 2)
-//                activityDAO.updateActivityBasedOnActivityId(activity3updated.id, activity3updated)
-//                assertEquals(activity3updated, activityDAO.findByActivityId(3))
-//            }
-//        }
-//
-//        @Test
-//        fun `updating non-existant activity in table results in no updates`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val activityDAO = populateActivityTable()
-//
-//                //Act & Assert
-//                val activity4updated = Activity(id = 4, description = "Cardio", duration = 42.0,
-//                    calories = 220, started = DateTime.now(), userId = 2)
-//                activityDAO.updateActivityBasedOnActivityId(4, activity4updated)
-//                assertEquals(null, activityDAO.findByActivityId(4))
-//                assertEquals(3, activityDAO.getAll().size)
-//            }
-//        }
-//    }
-//    @Nested
-//    inner class DeleteWorkouts {
-//
-//        @Test
-//        fun `deleting a non-existant workout (by id) in table results in no deletion`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val workoutDAO = populateWorkoutTable()
-//
-//                //Act & Assert
-//                assertEquals(3, workoutDAO.getAllWorkouts().size)
-//                workoutDAO.deleteWorkoutByWorkoutId(4)
-//                assertEquals(3, workoutDAO.getAllWorkouts().size)
-//            }
-//        }
-//
-//        @Test
-//        fun `deleting an existing activity (by id) in table results in record being deleted`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val activityDAO = populateActivityTable()
-//
-//                //Act & Assert
-////                assertEquals(3, activityDAO.getAll().size)
-////                activityDAO.deleteActivityByActivityId(activity3.id)
-////                assertEquals(2, activityDAO.getAll().size)
-//            }
-//        }
-//
-//
-//        @Test
-//        fun `deleting workouts when none exist for user id results in no deletion`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val workoutDAO = populateWorkoutTable()
-//
-//                //Act & Assert
-//                assertEquals(3, workoutDAO.getAllWorkouts().size)
-//                workoutDAO.deleteWorkoutByUserId(3)
-//                assertEquals(3, workoutDAO.getAllWorkouts().size)
-//            }
-//        }
-//
-//        @Test
-//        fun `deleting workout when 1 or more exist for user id results in deletion`() {
-//            transaction {
-//
-//                //Arrange - create and populate tables with three users and three activities
-//                val userDAO = populateUserTable()
-//                val workoutDAO = populateWorkoutTable()
-//
-//                //Act & Assert
-//                assertEquals(3, workoutDAO.getAllWorkouts().size)
-//                workoutDAO.deleteWorkoutByUserId(1)
-//                assertEquals(1, workoutDAO.getAllWorkouts().size)
-//            }
-//        }
-//    }
+    @Nested
+    inner class ReadWorkouts {
+
+        @Test
+        fun `getting all workouts stored in table successfully`(){
+            transaction {
+                val usersDAO = populateUserTable()
+                val workoutsDAO = populateWorkoutTable()
+                TestCase.assertEquals(3, workoutsDAO.getAllWorkouts().size)
+            }
+
+
+
+        }
+        @Test
+        fun `get workouts by user id that has no related workouts, results in no record found`(){
+            transaction {
+                val usersDAO = populateUserTable()
+                val workoutsDAO = populateWorkoutTable()
+                TestCase.assertEquals(0, workoutsDAO.findWorkoutByUserId(4).size)
+            }
+
+
+
+        }
+
+        @Test
+        fun `get workout by user id that has related workout, results in correct workout returned`() {
+            transaction {
+                val usersDAO = populateUserTable()
+               val workoutDAO = populateWorkoutTable()
+
+               TestCase.assertEquals(workout1, workoutDAO.findWorkoutByUserId(1).get(0))
+               TestCase.assertEquals(workout3, workoutDAO.findWorkoutByUserId(1).get(1))
+
+
+
+            }
+
+        }
+        @Test
+        fun `get all workout over empty table returns none`() {
+            transaction {
+
+
+                SchemaUtils.create(Workouts)
+                val workoutDAO = WorkoutDAO()
+
+                //Act & Assert
+                TestCase.assertEquals(0, workoutDAO.getAllWorkouts().size)
+            }
+        }
+        @Test
+        fun `get workout by workout id that has no records, results in no record returned`() {
+            transaction {
+
+                val userDAO = populateUserTable()
+                val workoutDAO = populateWorkoutTable()
+                //Act & Assert
+                TestCase.assertEquals(null, workoutDAO.findByWorkoutId(4))
+            }
+        }
+
+    }
+
+    @Nested
+    inner class UpdateWorkouts {
+
+        @Test
+        fun `updating existing workout in table results in successful update`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutsDAO = populateWorkoutTable()
+
+                //Act & Assert
+                val workoutupdated =  Workout(id=1,name = "asthetics", description = "fitness training", duration = 7.5, userId = 1,mincalories=200)
+                workoutsDAO.updateWorkoutBasedOnWorkoutId(workoutupdated.id, workoutupdated)
+                assertEquals(workoutupdated, workoutsDAO.findByWorkoutId(1))
+            }
+        }
+
+        @Test
+        fun `updating non-existant workout in table results in no updates`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutsDAO = populateWorkoutTable()
+
+                //Act & Assert
+                val workoutupdated =  Workout(id=6,name = "cardio", description = "Light weight training", duration = 7.5, userId = 1,mincalories=200)
+                workoutsDAO.updateWorkoutBasedOnWorkoutId(4, workoutupdated)
+                assertEquals(null, workoutsDAO.findByWorkoutId(4))
+
+            }
+        }
+    }
+    @Nested
+    inner class DeleteWorkouts {
+
+        @Test
+        fun `deleting a non-existant workout (by id) in table results in no deletion`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutDAO = populateWorkoutTable()
+
+                //Act & Assert
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+                workoutDAO.deleteWorkoutByWorkoutId(4)
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+            }
+        }
+
+        @Test
+        fun `deleting an existing workout (by id) in table results in record being deleted`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutDAO = populateWorkoutTable()
+                //Act & Assert
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+                workoutDAO.deleteWorkoutByWorkoutId(workout3.id)
+                assertEquals(2, workoutDAO.getAllWorkouts().size)
+            }
+        }
+
+
+        @Test
+        fun `deleting workouts when none exist for user id results in no deletion`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutDAO = populateWorkoutTable()
+
+                //Act & Assert
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+                workoutDAO.deleteWorkoutByUserId(3)
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+            }
+        }
+
+        @Test
+        fun `deleting workout when 1 or more exist for user id results in deletion`() {
+            transaction {
+
+
+                val userDAO = populateUserTable()
+                val workoutDAO = populateWorkoutTable()
+
+                //Act & Assert
+                assertEquals(3, workoutDAO.getAllWorkouts().size)
+                workoutDAO.deleteWorkoutByUserId(1)
+                assertEquals(1, workoutDAO.getAllWorkouts().size)
+            }
+        }
+    }
 
 }
