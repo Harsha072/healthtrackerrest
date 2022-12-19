dc69b17c6940207e72023c2eefe20b3916fe3eba
Harsha072 <20100308@mail.wit.ie>
2022-12-19 15:16:29 +0000

    Basic vue code re-added

---
+++ src/main/resources/vue/views/activity-overview.vue
@@ -0,0 +1,31 @@
+<template id="activity-overview">
+  <app-layout>
+    <div>
+      <div>
+        <ul class="activity-overview-list">
+          <li v-for="activities in activities">
+            <a :href="`/users/${activities.id}`">{{activities.name}} </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </app-layout>
+</template>
+<script>
+Vue.component("activity-overview", {
+  template: `#activity-overview`,
+  data: () => ({
+    activities: [],
+  }),
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    fetchUsers: function () {
+      axios.get("/api/activities")
+          .then(res => this.activities = res.data)
+          .catch(() => alert("Error while fetching users"));
+    }
+  }
+});
+</script>
